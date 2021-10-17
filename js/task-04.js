20603b6aa2b1ce6efcca06187ca6acf09e9d868b
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2021-10-17 21:24:15 +0300

    task 10

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,7 @@
 let counterValue = 0;
 const result = document.querySelector('#value');
-const decrButton = document.querySelector('[data-action="decrement"]');
-const icrButton = document.querySelector('[data-action="increment"]');
+const decrButton = document.querySelector('button[data-action="decrement"]');
+const icrButton = document.querySelector('button[data-action="increment"]');
 
 decrButton.addEventListener("click", () => {
    result.textContent = counterValue -= 1;
--- js/task-10.js
+++ js/task-10.js
@@ -2,9 +2,37 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const create = document.querySelector('[data-create]');
-const destroy = document.querySelector('[data-destroy]');
-const quantity = document.querySelector('input');
+const control = document.querySelector('#controls');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const input = document.querySelector('input');
 const result = document.querySelector('#boxes');
+
+const createBoxes = (amount) => {
+  amount = input.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    const basicSize = 30;
+    const biggestSize = basicSize + i * 10;
+    const box = document.createElement('div');
+    box.setAttribute(
+      'style',
+      `width:${biggestSize}px, height:${biggestSize}px, background-color:${getRandomHexColor()}`
+    );
+
+    result.append(box);
+      
+  } 
+}
+
+const destroyBoxes = () => {
+  input.value = '';
+  result.innerHTML = '';
+}
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+
 
 
